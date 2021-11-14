3856edbe124e16dc9a9a2e73c328aa5a383f2dca
mkirk13 <90644769+mkirk13@users.noreply.github.com>
2021-11-14 13:41:05 -0500

    java clean up

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 console.log('Hi there. Thanks for viewing my portfolio!')
 
-function menuToggle() {
-	var x = document.getElementById('myNavtoggle')
-	var isMobileDevice = window.innerWidth <= 640
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  var isMobileDevice = window.innerWidth <= 640
 
-	if (x.className === 'navtoggle' && isMobileDevice) {
-		x.className += ' responsive'
-	} else {
-		x.className = 'navtoggle'
-	}
+  if (x.className === 'navtoggle' && isMobileDevice) {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
